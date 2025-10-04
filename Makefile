d2eb631f04c0c1e081b6831bd32d1bd15fe077d7
Alvin Häger <alvinhager@hotmail.com>
2025-10-04 10:59:19 +0200

    feat: updated website to be able to deploy to gh-pages

--- Makefile
+++ Makefile
@@ -5,3 +5,11 @@ run-dev:
 PHONY: install
 install: 
 	pnpm install
+
+PHONY: deploy
+deploy:
+	pnpm run deploy
+
+PHONY: build
+build:
+	pnpm build
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://alvinhager.github.io/alvinh/",
   server: {
     host: "localhost",
     port: 3000,
